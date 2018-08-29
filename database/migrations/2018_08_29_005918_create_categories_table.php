6e615eb495b873c405d734d2bb4c83fb9b61faf5
BenjaMolina <molinasb.94@gmail.com>
2018-08-28 20:25:23 -0500

    3.- campos para la migracion categorias y defaultStringLength fixed

--- database/migrations/2018_08_29_005918_create_categories_table.php
+++ database/migrations/2018_08_29_005918_create_categories_table.php
@@ -15,6 +15,11 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->increments('id');
+
+            $table->string('name');
+            $table->string('slug')->unique(); //URL amigable
+            $table->mediumText('body')->nullable();
+
             $table->timestamps();
         });
     }
