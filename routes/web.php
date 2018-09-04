54fdba040293464605dd725d2abe9db49d4dfedd
BenjaMolina <molinasb.94@gmail.com>
2018-09-03 20:00:38 -0500

    9.- Modulo Administrativo - Estructura Controladores, vistas (Tag)

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,13 @@ Route::get('/', function () {
 
 Auth::routes();
 
+//Web
 Route::get('blog', 'Blog\PagesController@blog')->name('blog');
 Route::get('post/{slug}', 'Blog\PagesController@post')->name('post');
 Route::get('categoria/{slug}', 'Blog\PagesController@categoria')->name('categoria');
 Route::get('etiqueta/{slug}', 'Blog\PagesController@etiqueta')->name('etiqueta');
+
+//Administrador
+Route::resource('tags', 'Admin\TagController');
+Route::resource('categories', 'Admin\CategoryController');
+Route::resource('posts', 'Admin\PostController');
